a9f380ed32cf5df6cfda86a80deaf1fcd3e841e9
Sandstorm <sandstorm.art@yandex.ru>
2024-10-28 19:46:57 +0300

    cmitt 4

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-print("Summ a + b please:\n")
-a, b = map(int, input().split())
-print((a + b) * 2)
+print("Summ (a + b + c) * 2 please:\n")
+a, b, c = map(int, input().split())
+print(((a + b + c) * 2))
